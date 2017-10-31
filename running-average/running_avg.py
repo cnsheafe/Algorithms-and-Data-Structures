f2f854a8f40822635786316b46a88b9dfa2f21ec
cnsheafe <cnsheafe@gmail.com>
2017-10-31 14:54:03 -0600

    added cumulative average

--- running-average/running_avg.py
+++ running-average/running_avg.py
@@ -1,29 +1,41 @@
+'''Collection of different running averages'''
+
+
 def simple_avg(data_series, window_length):
+    ''' Returns the simple running average.
+    Arguments:
+        data_series -- Array of numerical values
+        window_length -- Integer describing how many values to include
+    '''
     averaged_series = []
     init_sum = 0
     for i in range(0, window_length):
         init_sum += data_series[i]
-    averaged_series.append(init_sum/window_length)
+    averaged_series.append(init_sum / float(window_length))
     for i in range(window_length, len(data_series)):
         new_term = data_series[i]
-        old_term = data_series[i-window_length]
-        next_avg = averaged_series[len(averaged_series)-1] + (new_term - old_term)/float(window_length)
+        old_term = data_series[i - window_length]
+        next_avg = averaged_series[len(
+            averaged_series) - 1] + (new_term - old_term) / float(window_length)
         averaged_series.append(next_avg)
     return averaged_series
 
-def center_avg(data_series, window_length):
+
+def cumulative_avg(data_series, starting_point):
+    '''Returns the cumulative running average.
+    Arguments:
+        data_series -- Array of numerical values
+        starting_point -- Indice in data_series to start averaging
+    '''
     averaged_series = []
     init_sum = 0
-    L = window_length/2
-    for i in range(0, window_length):
+    for i in range(0, starting_point):
         init_sum += data_series[i]
-    averaged_series.append(init_sum/window_length)
-    for i in range(L + 1, len(data_series) - L):
-        new_term = data_series[i+1, i+L]
-        old_term = data_series[i-L]
-        next_avg = averaged_series[len(averaged_series)-1] + (new_term - old_term)/window_length
+    averaged_series.append(init_sum / float(starting_point))
+    for i in range(starting_point, len(data_series)):
+        next_avg = (
+            data_series[i] + i * averaged_series[len(averaged_series) - 1]) / float(i + 1)
         averaged_series.append(next_avg)
     return averaged_series
-myAvg = simple_avg([1,5,3,2,10,12,8,6,22,9,11], 3)
-myCenterAvg = center_avg([1,5,3,2,10,12,8,6,22,9,11], 3)
-print(myCenterAvg)
+
+x = cumulative_avg([1,2,3,4,1,6,5], 2)
